e7731ace9aed19a8d4de98e1a6491e9c153598ce
ramona-2020 <ramona.gospodinova@plana.solutions>
2022-09-24 12:22:38 +0300

    DEBUG = False

--- my_music_app/settings.py
+++ my_music_app/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6!g21(x5cb86@31vntvu@6ojgzr7fu*7f^=6sd@&aq@_vgb%w5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
