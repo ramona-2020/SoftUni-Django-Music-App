f740ded3b19f5834b2480a91243877fd14ef3461
ramona-2020 <ramona.gospodinova@plana.solutions>
2022-09-24 12:26:33 +0300

    Automatic configuration with django-heroku

--- my_music_app/settings.py
+++ my_music_app/settings.py
@@ -110,3 +110,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
